f69bb305079a4fa6fa6d1846a8e8b8315267e23a
DeadMarshes <p1kster@hotmail.com>
2023-12-11 17:59:49 -0800

    Fixes on sales pk and other small references

--- FinalProject.sql
+++ FinalProject.sql
@@ -5,6 +5,9 @@ drop table [dbo].[G5_CHANNELS]
 drop table [dbo].G5_CUSTOMERS
 drop table [dbo].G5_PRODUCTS
 drop table [dbo].G5_PROMOTIONS
+drop table [dbo].G5_COUNTRY
+drop table dbo.G5_CusEx
+drop table dbo.G5_CusIn
 
 --  1. G5_CHANNELS Table
 --- 1.1 create table
@@ -21,17 +24,16 @@ SELECT [CHANNEL_ID], [CHANNEL_DESC], [CHANNEL_DESC] FROM [dbo].[LI_CHANNELS]
 
 -- 2. Create G5_Customer Table with the data field definition we want
 --- 2.1 Create G5_CusEx table
-select * into
-G5_CusEx from
-[dbo].[LI_CUSTOMERS_EXT]
+
+select * into G5_CusEx
+from [dbo].[LI_CUSTOMERS_EXT]
 
 --- 2.2 Create G5_CusIn table
-select * into
-G5_CusIn from
-[dbo].[LI_CUSTOMERS_INTX]
+select * into G5_CusIn
+from [dbo].[LI_CUSTOMERS_INTX]
 
 --- 2.3 Join tables together
-select cx.[CUST_ID], [CUST_FIRST_NAME],[CUST_LAST_NAME], 
+select cx.[CUST_ID], [CUST_FIRST_NAME],[CUST_LAST_NAME],
 	cx.[CUST_GENDER],[CUST_YEAR_OF_BIRTH],[CUST_MARITAL_STATUS],
 	[CUST_INCOME_LEVEL],[CUST_CREDIT_LIMIT],
 	[CUST_MAIN_PHONE_NUMBER],[CUST_EMAIL],
@@ -44,7 +46,7 @@ into G5_CUSTOMERS
 from G5_CusEx cx
 join G5_CusIn ci on cx.CUST_ID = ci.CUST_ID
 
---- 2.4 Table clean up 
+--- 2.4 Table clean up
 ---- CONSTRAINT_MARITAL_STATUS
 --- widow, divorced, single, married, separated, null, 5 migtht be space
 -- case value to clean up marital status
@@ -65,8 +67,8 @@ set CUST_MARITAL_STATUS = 'married'
 where CUST_MARITAL_STATUS = 'Mar-AF'
 
 update G5_CUSTOMERS
-set CUST_MARITAL_STATUS = 'widow'
-where CUST_MARITAL_STATUS = 'Widowed'
+set CUST_MARITAL_STATUS = 'widowed'
+where CUST_MARITAL_STATUS = 'Widowed' OR CUST_MARITAL_STATUS = 'Widow'
 
 update G5_CUSTOMERS
 set CUST_MARITAL_STATUS = 'separated'
@@ -99,14 +101,19 @@ alter table G5_CUSTOMERS drop column COUNTRY_TOTAL;
 alter table G5_CUSTOMERS drop column CUST_TOTAL;
 alter table G5_CUSTOMERS ADD CONSTRAINT PK_CUST_ID PRIMARY KEY clustered (CUST_ID);
 alter table G5_CUSTOMERS ADD CONSTRAINT CONSTRAINT_GENDER CHECK (CUST_GENDER='f' OR CUST_GENDER='m' OR CUST_GENDER='F' OR CUST_GENDER='M');
-alter table G5_CUSTOMERS ADD CONSTRAINT CONSTRAINT_MARITAL_STATUS CHECK (CUST_MARITAL_STATUS='single' OR  CUST_MARITAL_STATUS='married' OR CUST_MARITAL_STATUS='divorced'OR CUST_MARITAL_STATUS='separated'OR CUST_MARITAL_STATUS='widow'OR CUST_MARITAL_STATUS is null);
+alter table G5_CUSTOMERS ADD CONSTRAINT CONSTRAINT_MARITAL_STATUS CHECK (CUST_MARITAL_STATUS='single' OR  CUST_MARITAL_STATUS='married' OR CUST_MARITAL_STATUS='divorced'OR CUST_MARITAL_STATUS='separated' OR CUST_MARITAL_STATUS='widowed' OR CUST_MARITAL_STATUS is null);
 
 
 -- 3. Create Country table from G5_CUSTOMERS
-select DISTINCT COUNTRY_ID, COUNTRY_NAME, COUNTRY_SUBREGION, COUNTRY_REGION
+go
+select DISTINCT [COUNTRY_ID], COUNTRY_NAME, COUNTRY_SUBREGION, COUNTRY_REGION
 into G5_COUNTRY
 from G5_CUSTOMERS
+go
+alter table G5_COUNTRY alter column COUNTRY_ID int not null
+go
 alter table G5_COUNTRY ADD CONSTRAINT CONSTRAINT_COUNTRY_REGION CHECK ([COUNTRY_REGION]='Oceania' OR [COUNTRY_REGION]='Europe' OR [COUNTRY_REGION]='Americas' OR [COUNTRY_REGION]='Africa' OR [COUNTRY_REGION]='Asia');
+alter table G5_COUNTRY ADD CONSTRAINT PK_COUNTRY_ID PRIMARY KEY clustered (COUNTRY_ID);
 -- 3.1 Delete the country columns from G5_CUSTOMERS
 alter table G5_CUSTOMERS drop column COUNTRY_ID;
 alter table G5_CUSTOMERS drop column COUNTRY_NAME;
@@ -140,7 +147,7 @@ alter table G5_PRODUCTS alter column PROD_WEIGHT_CLASS int not NULL;
 alter table G5_PRODUCTS alter column PROD_LIST_PRICE decimal(20, 2) not NULL;
 alter table G5_PRODUCTS ADD CONSTRAINT PK_PROD_ID PRIMARY KEY clustered (PROD_ID);
 
--- 5. Create Promotions Table 
+-- 5. Create Promotions Table
 --- 5.1 create and populate Promotions table
 select * into
 G5_PROMOTIONS from
@@ -156,7 +163,7 @@ alter table G5_PROMOTIONS alter column PROMO_BEGIN_DATE date null;
 alter table G5_PROMOTIONS alter column PROMO_END_DATE date not null;
 alter table G5_PROMOTIONS DROP column PROMO_TOTAL;
 
-alter table G5_PROMOTIONS ADD CONSTRAINT PK_PROMO_ID PRIMARY KEY clustered (PROMO_ID) 
+alter table G5_PROMOTIONS ADD CONSTRAINT PK_PROMO_ID PRIMARY KEY clustered (PROMO_ID)
 
 -- 6. Create Sales Table
 --- 6.1 Create G5_SALES table
@@ -165,12 +172,12 @@ select * into
 G5_SALES from
 (select * from [dbo].[LI_SALES_12_13]
 union
-select * from [dbo].[LI_SALES_14]) a 
+select * from [dbo].[LI_SALES_14]) a
 order by [SALESTRANS_ID]
 
-
---- 6.2 Table clean up 
-alter table G5_SALES ADD CONSTRAINT PK_CUST_ID PRIMARY KEY clustered (CUST_ID);
+go
+--- 6.2 Table clean up
+-- alter table G5_SALES ADD CONSTRAINT PK_CUST_ID PRIMARY KEY clustered (CUST_ID);
 
 alter table G5_SALES alter column SALESTRANS_ID integer not null;
 alter table G5_SALES alter column PROD_ID integer not null;
@@ -181,6 +188,7 @@ alter table G5_SALES alter column SALE_DATE date null;
 alter table G5_SALES alter column SHIPPING_DATE date null;
 alter table G5_SALES alter column PAYMENT_DATE date null;
 alter table G5_SALES alter column QUANTITY_SOLD integer not null;
+go
 sp_rename 'G5_SALES.AMOUNT_SOLD','TOTAL_PRICE','COLUMN';
 alter table G5_SALES alter column TOTAL_PRICE decimal(20,2) not null; 
 alter table G5_SALES alter column UNIT_PRICE decimal(20,2) not null;
